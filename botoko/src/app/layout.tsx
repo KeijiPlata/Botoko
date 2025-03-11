248ca7a809ffa94485e922455e38895b4a4d37b6
KeijiPlata <lordmiackykeijip@gmail.com>
2025-03-11 21:15:57 +0800

    feat: add header design, install necessary packages

--- botoko/src/app/layout.tsx
+++ botoko/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Poppins } from "next/font/google";
 import Header from "./components/Header";
 import "./globals.css";
@@ -9,16 +8,6 @@ const poppins = Poppins({
   weight: ["400", "600"],
   display: "swap",
   variable: "--font-poppins",
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -34,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} ${geistMono.variable} ${geistSans.variable} antialiased`}
+        className={`${poppins.variable} antialiased`}
       >
         <Header />
         {children}
