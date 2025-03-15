0da0376ccb72d168fa984fd1fed1657f8f15b81c
KeijiPlata <lordmiackykeijip@gmail.com>
2025-03-15 19:33:00 +0800

    feat: add responsive tabs

--- botoko/src/app/candidates/page.tsx
+++ botoko/src/app/candidates/page.tsx
@@ -2,26 +2,50 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { FiSearch } from "react-icons/fi";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function CandidatesPage() {
   return (
-    <div className="font-poppins">
-      {" "}
-      <div className="flex w-full max-w-lg items-center gap-3">
-        <div className="relative w-full max-w-sm">
-          <FiSearch
-            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
-            size={18}
-          />
-          <Input
-            type="text"
-            placeholder="Search..."
-            className="pl-10" // Add padding to prevent overlap
-          />
+    <Tabs defaultValue="all" className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 md:gap-3 md:flex-row md:justify-between items-center w-full">
+        <div className="flex w-full max-w-lg items-center gap-3">
+          <div className="relative w-full max-w-sm">
+            <FiSearch
+              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+              size={18}
+            />
+            <Input type="text" placeholder="Search..." className="pl-10" />
+          </div>
+          <Button type="submit" className="bg-custom-blue hover:bg-sky-900">
+            Search
+          </Button>
         </div>
-        <Button type="submit" className="bg-custom-blue">Search</Button>
+
+        <TabsList className="flex max-w-sm w-full">
+          <TabsTrigger
+            value="candidates"
+            className="text-gray-500 data-[state=active]:bg-custom-blue data-[state=active]:text-white px-4 py-2 rounded-md transition w-full"
+          >
+            Candidates
+          </TabsTrigger>
+          <TabsTrigger
+            value="myvotes"
+            className="text-gray-500 data-[state=active]:bg-custom-blue data-[state=active]:text-white px-4 py-2 rounded-md transition w-full"
+          >
+            My Votes
+          </TabsTrigger>
+        </TabsList>
       </div>
-    </div>
+
+      <div className="w-full p-4 rounded-md">
+        <TabsContent value="candidates">
+          <p>All candidates will be displayed here.</p>
+        </TabsContent>
+        <TabsContent value="myvotes">
+          <p>Shortlisted candidates will be displayed here.</p>
+        </TabsContent>
+      </div>
+    </Tabs>
   );
 }
 
