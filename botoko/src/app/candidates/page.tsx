cb338b5172b5cab70d8f4a891461164e707e1623
KeijiPlata <lordmiackykeijip@gmail.com>
2025-03-16 20:42:51 +0800

    feat: design candidate component for large screen only

--- botoko/src/app/candidates/page.tsx
+++ botoko/src/app/candidates/page.tsx
@@ -3,11 +3,16 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { FiSearch } from "react-icons/fi";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import sample from "../../../public/candidates/Abalos.png";
+import sample2 from "../../../public/candidates/Adonis.png";
+
+
+import Candidate from "../components/Candidate";
 
 function CandidatesPage() {
   return (
-    <Tabs defaultValue="all" className="flex flex-col gap-5">
-      <div className="flex flex-col gap-5 md:gap-3 md:flex-row md:justify-between items-center w-full">
+    <Tabs defaultValue="candidates" className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 md:gap-3 md:flex-row md:justify-between items-center w-full mb-7">
         <div className="flex w-full max-w-lg items-center gap-3">
           <div className="relative w-full max-w-sm">
             <FiSearch
@@ -37,9 +42,10 @@ function CandidatesPage() {
         </TabsList>
       </div>
 
-      <div className="w-full p-4 rounded-md">
-        <TabsContent value="candidates">
-          <p>All candidates will be displayed here.</p>
+      <div className="w-full  rounded-md">
+        <TabsContent value="candidates" className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          <Candidate firstName="Benhur" lastName="Abalos" position="Candidate for Senate 2025" image={sample.src} isAdded={false} />
+          <Candidate firstName="Jerome" lastName="Adonis" position="Candidate for Senate 2025" image={sample2.src} isAdded={false} />
         </TabsContent>
         <TabsContent value="myvotes">
           <p>Shortlisted candidates will be displayed here.</p>
--- botoko/src/app/components/Candidate.tsx
+++ botoko/src/app/components/Candidate.tsx
@@ -1,25 +1,49 @@
 import React from "react";
 import Image from "next/image";
+import { IoMdAdd } from "react-icons/io";
+import { FaMinus } from "react-icons/fa";
 
 type CandidateProps = {
-  name: string;
+  firstName: string;
+  lastName: string;
   position: string;
   image: string;
   isAdded: boolean;
 };
 
 const Candidate = ({
-  name,
+  firstName,
+  lastName,
   position,
   image,
   isAdded,
 }: CandidateProps): React.JSX.Element => {
   return (
-    <div className="bg-custom-blue flex flex-row justify-center w-full">
-      <h2>{name}</h2>
-      <p>{position}</p>
-      <Image src={image} alt={name} height={300} width={300}/>
-      <button>{isAdded ? "Added" : "Add Candidate"}</button>
+    <div className=" bg-custom-blue rounded-lg w-full shadow-md grid grid-cols-3 justify-items-center content-center place-items-center place-content-center relative h-36">
+      <Image
+        src={image}
+        width={200}
+        height={200}
+        alt=""
+        className="absolute left-5 bottom-0"
+      />
+
+      <div className="col-span-2 justify-self-end">
+        <h2 className="text-white leading-none font-semibold text-lg uppercase">
+          {firstName}
+        </h2>
+        <h2 className="text-white text-6xl font-black uppercase leading-none">
+          {lastName}
+        </h2>
+        <p className="leading-none text-white italic">{position}</p>
+      </div>
+      <div className="justify-self-end pr-4">
+        {isAdded ? (
+          <FaMinus className="text-5xl text-white" />
+        ) : (
+          <IoMdAdd className="text-5xl text-white" />
+        )}
+      </div>
     </div>
   );
 };
