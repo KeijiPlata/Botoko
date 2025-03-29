10d5de6cdb7358b680a9e984dbeba0e926053b8e
KeijiPlata <lordmiackykeijip@gmail.com>
2025-03-29 15:42:39 +0800

    chore: put the logo, and make it responsive to all devices

--- botoko/src/app/final/page.tsx
+++ botoko/src/app/final/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState } from "react";
 import { FinalCandidate } from "../components/FinalCandidate";
+import Image from "next/image";
+import logo from "../../../public/logo-botoko-with-tagline.svg";
 
 type CandidateType = {
   id: number;
@@ -16,7 +18,8 @@ const FinalListPage = () => {
   });
 
   return (
-    <div className="pb-4 w-full">
+    <div className="pb-4 w-full flex flex-col gap-3">
+      <Image src={logo} className="self-center w-64 md:w-72 lg:w-80 h-auto" alt="logo"/>
       <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-y-3 gap-1 place-items-center">
         {[...Array(12)].map((_, index) => (
           <FinalCandidate key={index} />
