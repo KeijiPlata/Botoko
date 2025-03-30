eb66c91d7b6f46db4153e4e037321272f5395d10
KeijiPlata <lordmiackykeijip@gmail.com>
2025-03-30 20:17:28 +0800

    feature: add share button and edit button

--- botoko/src/app/final/page.tsx
+++ botoko/src/app/final/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { useState } from "react";
 import { FinalCandidate } from "../components/FinalCandidate";
+import { IoMdShare } from "react-icons/io";
+import { FaEdit } from "react-icons/fa";
+import { IoEllipsisVertical } from "react-icons/io5";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import Image from "next/image";
 import logo from "../../../public/logo-botoko-with-tagline.svg";
 
@@ -20,11 +25,42 @@ const FinalListPage = () => {
 
   return (
     <div className="pb-4 w-full flex flex-col gap-3">
-      <Image
-        src={logo}
-        className="self-center w-64 md:w-72 lg:w-80 h-auto"
-        alt="logo"
-      />
+      <div className="w-full flex items-center">
+        <div className="flex-1"></div>
+        <Image src={logo} className="w-64 md:w-72 lg:w-80 h-auto" alt="logo" />
+
+        <div className="hidden sm:flex flex-1 justify-end lg:gap-3 md:gap-1 font-poppins">
+          <Button variant="ghost" className="text-lg text-gray-500">
+            <IoMdShare className="text-2xl cursor-pointer text-gray-500" />
+            Share
+          </Button>
+          <Button variant="ghost" className="text-lg text-gray-500">
+            <FaEdit className="text-2xl cursor-pointer text-gray-500" />
+            Edit
+          </Button>
+        </div>
+
+        <div className="flex sm:hidden flex-1 justify-end font-poppins">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="text-gray-500">
+                <IoEllipsisVertical className="text-2xl cursor-pointer text-gray-500" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem className="flex items-center gap-2 text-gray-500">
+                <IoMdShare className="text-xl text-gray-500" />
+                Share
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex items-center gap-2 text-gray-500">
+                <FaEdit className="text-xl text-gray-500" />
+                Edit
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+
       <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-y-3 gap-1 place-items-center">
         {myVotes.map((candidate) => (
           <FinalCandidate
